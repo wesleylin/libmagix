2e404c5479ae9cadb4f70734af226586d0cb3520
Wesley Lin <wesleylin625@gmail.com>
2026-01-03 09:41:54 -0800

    clarify parent and child nodes

--- libmagix.go
+++ libmagix.go
@@ -5,22 +5,18 @@ import "github.com/wesleylin/libmagix/internal/parser"
 // MatchTree recursively walks the rule tree.
 // It returns the most specific (deepest) match found.
 func MatchTree(data []byte, rules []parser.Rule) *parser.Rule {
-	var bestMatch *parser.Rule
 
-	for _, rule := range rules {
-		if rule.Match(data) {
-			// If it matches, we assume this is our best match for now...
-			bestMatch = &rule
-
-			// ...BUT, we must check if any of its children match.
+	for i := range rules {
+		if rules[i].Match(data) {
+			// if any children match, prefer them.
 			// Children are more specific (e.g., "Zip" -> "DocX").
-			childMatch := MatchTree(data, rule.Children)
+			childMatch := MatchTree(data, rules[i].Children)
 			if childMatch != nil {
 				return childMatch
 			}
 
 			// If no children match, this parent is the best we've got.
-			return bestMatch
+			return &rules[i]
 		}
 	}
 
