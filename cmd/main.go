d7660324c8b5f3aea10997c73f4392c35fce9502
Wesley Lin <wesleylin625@gmail.com>
2026-01-03 10:03:03 -0800

    add cmd

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/wesleylin/libmagix"
+)
+
+func main() {
+	// 1. Point to your Magdir folder
+	m, err := libmagix.New("./magic/Magdir")
+	if err != nil {
+		fmt.Printf("Error loading magic files: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: magix <file>")
+		return
+	}
+	// 2. Read file
+	filePath := os.Args[1]
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	// 3. Identify!
+	result := m.Identify(data)
+	if result != nil {
+		fmt.Printf("File Type: %s (MIME: %s)\n", result.Message, result.Mime)
+	} else {
+		fmt.Println("File type not identified.")
+	}
+}
--- libmagix.go
+++ libmagix.go
@@ -2,6 +2,24 @@ package libmagix
 
 import "github.com/wesleylin/libmagix/internal/parser"
 
+type Magix struct {
+	rules []parser.Rule
+}
+
+// New loads all magic files from the specified directory.
+func New(magicDir string) (*Magix, error) {
+	rules, err := parser.LoadDirectory(magicDir)
+	if err != nil {
+		return nil, err
+	}
+	return &Magix{rules: rules}, nil
+}
+
+// Identify takes file bytes and returns the match.
+func (m *Magix) Identify(data []byte) *parser.Rule {
+	return MatchTree(data, m.rules)
+}
+
 // MatchTree recursively walks the rule tree.
 // It returns the most specific (deepest) match found.
 func MatchTree(data []byte, rules []parser.Rule) *parser.Rule {
